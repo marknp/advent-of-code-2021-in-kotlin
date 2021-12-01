f60b7f025a6a30e4b894f3929ad20f408675183f
Marion Knopp <marion.knopp@tngtech.com>
2021-12-01 08:45:03 +0100

    Day 1 part 1.
    My first kotlin code!

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,6 +1,15 @@
+import java.util.stream.IntStream
+
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        val inputAsNumbers = input.map { string -> Integer.valueOf(string) }
+        var sum = 0
+        for (i in IntStream.rangeClosed(1, input.size - 1)) {
+            if (inputAsNumbers[i] > inputAsNumbers[i - 1]) {
+                sum++
+            }
+        }
+        return sum
     }
 
     fun part2(input: List<String>): Int {
@@ -9,9 +18,9 @@ fun main() {
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 7)
 
     val input = readInput("Day01")
-    println(part1(input))
-    println(part2(input))
+    println("Part 1: " + part1(input))
+    println("Part 1: " + part2(input))
 }
