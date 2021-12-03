e55a0ffc5ca7233748bf08a7e305a559f197fa19
Marion Knopp <marion.knopp@tngtech.com>
2021-12-03 08:50:10 +0100

    refactor(d1p1): use until

--- src/main/kotlin/de/marion/aoc2021/Day03.kt
+++ src/main/kotlin/de/marion/aoc2021/Day03.kt
@@ -1,12 +1,39 @@
 private const val DAY = 3
-private const val PART_ONE_TEST_RESULT = 150
-private const val PART_TWO_TEST_RESULT = 900
+private const val PART_ONE_TEST_RESULT = 198
+private const val PART_TWO_TEST_RESULT = -1
 
-private const val PART_ONE_ACTUAL_RESULT = -1
+private const val PART_ONE_ACTUAL_RESULT = 3374136
 private const val PART_TWO_ACTUAL_RESULT = -1
 
 private fun part1(input: List<String>): Int {
-    throw NotImplementedError("TODO Part 1")
+    val gamma = gamma(input)
+    val eps = eps(input)
+    return gamma * eps
+
+}
+
+private fun gamma(input: List<String>): Int {
+    val len = input.size
+    val s = (0 until input[0].length)
+        .map { i ->
+            input.map { inp -> inp[i].toString().toInt() }
+                .sum() > (len / 2)
+        }.map { it.compareTo(false) }
+        .joinToString("")
+
+    return Integer.parseInt(s, 2)
+}
+
+private fun eps(input: List<String>): Int {
+    val len = input.size
+    val s = (0 until input[0].length)
+        .map { i ->
+            input.map { inp -> inp[i].toString().toInt() }
+                .sum() < (len / 2)
+        }.map { it.compareTo(false) }
+        .joinToString("")
+
+    return Integer.parseInt(s, 2)
 }
 
 private fun part2(input: List<String>): Int {
