b2d2920ed20b6efe27fe7332902db97581bc81e2
Marion Knopp <marion.knopp@tngtech.com>
2021-12-04 11:11:53 +0100

    feat(d4)

--- src/main/kotlin/de/marion/aoc2021/Day04.kt
+++ src/main/kotlin/de/marion/aoc2021/Day04.kt
@@ -1,16 +1,56 @@
-private const val DAY = 3
-private const val PART_ONE_TEST_RESULT = 150
-private const val PART_TWO_TEST_RESULT = 900
+import java.lang.Integer.min
 
-private const val PART_ONE_ACTUAL_RESULT = -1
-private const val PART_TWO_ACTUAL_RESULT = -1
+private const val DAY = 4
+private const val PART_ONE_TEST_RESULT = 4512
+private const val PART_TWO_TEST_RESULT = 1924
+
+private const val PART_ONE_ACTUAL_RESULT = 58838
+private const val PART_TWO_ACTUAL_RESULT = 6256
 
 private fun part1(input: List<String>): Int {
-    throw NotImplementedError("TODO Part 1")
+    val parsed = parseData(input)
+    val fields = parsed.second
+    val moves = parsed.first
+    val firstWins = fields.map { it.findFirstWin(moves) }
+    val winningMove = firstWins.minOf { it }
+    val winningField = fields.get(firstWins.indexOf(winningMove))
+    return moves[winningMove] * winningField.getValue(moves.subList(0, moves.find { it.equals(winningMove) }!! +1))
 }
 
+fun parseData(input: List<String>): Pair<List<Int>, List<BingoField>> {
+    val moves = input.get(0).split(",").map { it.toInt() }
+
+    val bingos = input.subList(2, input.size)
+        .map { line -> line.split(" ").filter { it.isNotBlank() }.map { it.toInt() } }
+        .filter { it.isNotEmpty() }
+    val n = bingos.get(0).size
+    val fields = bingos.chunked(n).map { BingoField(it) }
+    return Pair(moves, fields)
+}
+
+class BingoField(private val field: List<List<Int>>) {
+    fun findFirstWin(moves: List<Int>): Int {
+        val firstmoves = field.map { line -> line.map { moves.indexOf(it) } }
+        val rowWinRounds = firstmoves.map { row -> row.maxOf { it } }
+        val colWinRounds = (field.indices).map { i -> firstmoves.maxOf { it[i] } }
+        return min(rowWinRounds.minOf { it }, colWinRounds.minOf { it })
+    }
+
+    fun getValue(moves: List<Int>): Int {
+        val v: List<Int> =  field.flatten().minus(moves.toSet())
+        return v.sum()
+    }
+}
+
+
 private fun part2(input: List<String>): Int {
-    throw NotImplementedError("TODO Part 2")
+    val parsed = parseData(input)
+    val fields = parsed.second
+    val moves = parsed.first
+    val firstWins = fields.map { it.findFirstWin(moves) }
+    val winningMove = firstWins.maxOf { it }
+    val winningField = fields.get(firstWins.indexOf(winningMove))
+    return moves[winningMove] * winningField.getValue(moves.subList(0, moves.find { it.equals(winningMove) }!! +1))
 }
 
 fun main() {
